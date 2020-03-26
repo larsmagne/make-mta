a1f38431f2a6fda8069d6b95688868e7afe3aa1e
Lars Ingebrigtsen <larsi@gnus.org>
2020-03-26 07:53:51 +0100

    Continue developing

--- make-mta.sh
+++ make-mta.sh
@@ -58,10 +58,94 @@ function certbot() {
     certbot certonly --standalone -d $host
 }
 
+function exim() {
+    apt install exim4-daemon-light
+    cat <<EOF > /etc/exim4/conf.d/main/00_tls_macros
+MAIN_TLS_ENABLE=yes
+MAIN_TLS_PRIVATEKEY=/etc/letsencrypt/live/$host/privkey.pem
+MAIN_TLS_CERTIFICATE=/etc/letsencrypt/live/$host/fullchain.pem
+EOF
+    sed -i "s/dc_use_split_config='false'/dc_use_split_config='true'/" \
+	/etc/exim4/update-exim4.conf.conf
+    sed -i "s/dc_relay_domains=''/dc_relay_domains='$domain'/" \
+	/etc/exim4/update-exim4.conf.conf
+    sed -i "s/dc_eximconfig_configtype='local'/dc_eximconfig_configtype='internet'/" \
+	/etc/exim4/update-exim4.conf.conf
+    sed -i "s/dc_local_interfaces='127.0.0.1 ; ::1'/dc_local_interfaces=''/" \
+	/etc/exim4/update-exim4.conf.conf
+
+    echo "daemon_smtp_ports = smtp : 587" > /etc/exim4/conf.d/main/00_ports
+
+    cat <<"EOF" > /etc/exim4/conf.d/auth/10_plain_server
+plain_server:
+  driver = plaintext
+  public_name = PLAIN
+  server_condition = "${if crypteq{$auth3}{${extract{1}{:}{${lookup{$auth2}lsea\
+rch{/etc/shadow}{$value}}}}}{1}{0}}"
+  server_set_id = $auth2
+  server_prompts = :
+  server_advertise_condition = ${if eq{$received_port}{587}{${if eq{$tls_in_cip\
+her}{}{no}{yes}}}{no}}
+EOF
+    
+    update-exim4.conf
+    service exim4 restart
+}
+
+function dkim() {
+    cd /etc/exim4
+    
+    # Generate private and public keys.
+    openssl genrsa -out "$domain-dkim-private.pem" 2048
+    openssl rsa -in "$domain-dkim-private.pem" -out \
+	    "$domain-dkim-public.pem" -pubout
+
+    # Allow exim to read the file.
+    chmod g+r "$domain-dkim-private.pem"
+    chgrp Debian-exim "$domain-dkim-private.pem"
+
+    selector=$(date +%Y%d%m)
+
+    cat <<EOF > conf.d/main/00_dkim_macros
+DKIM_CANON = relaxed
+DKIM_SELECTOR = $selector
+DKIM_DOMAIN = $domain
+DKIM_PRIVATE_KEY = /etc/exim4/$domain-dkim-private.pem
+EOF
+    update-exim4.conf
+    service exim4 reload
+
+    echo "Make the following TXT DNS record for $selector._domainkey.$domain"
+    echo
+    echo -n "  k=rsa; p="
+    grep -v '^-' < "$domain-dkim-public.pem" | tr -d '\n'
+    echo
+}
+
+function dns() {
+    echo "Make the following TXT DNS record for $domain"
+    echo
+    echo "  v=spf1 a mx ~all"
+    echo
+    echo "Make the following TXT DNS record for _dmarc.$domain"
+    echo
+    echo "  v=DMARC1; p=none"
+    echo
+    echo "Make the following MX DNS record for $domain"
+    echo
+    echo "  $host"
+}
+
 #preinstall
 #firewall
 sethost
 
 echo "Configuring for $host..."
 
+domain=$(echo $host | sed 's/^[^.]*[.]//')
+
 #certbot
+#exim
+#dkim
+
+dns
